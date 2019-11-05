0b2114927ff9e82aba5cf0c7718284b831cbe030
JerryG09 <phemmy09@gmail.com>
2019-11-05 07:54:27 +0100

    implemented search

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,8 +1,10 @@
 const graphql = require('graphql')
+const _ = require('lodash')
+
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
 
 // fake db
-const bookLog = [
+const books = [
     {name: "Great mind", genre: "Moltivativational", id: "1"},
     {name: "High mind", genre: "Sci-fi", id: "2"},
     {name: "Lovely mind", genre: "Fantasy", id: "3"}
@@ -24,6 +26,7 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLString}},
             resolve(parent, args) {
                 // Code to get data from db/ other source
+               return _.find(books, {id: args.id})
             }
         }
     }
