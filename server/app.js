1a85f2833b6800a5d1be5f8288e922c5deca7c5a
JerryG09 <phemmy09@gmail.com>
2019-11-05 07:10:09 +0100

    added express-graphql

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 import express from "express"
+import graphqlHTTP from 'express-graphql'
 
 const app = express()
+
+app.use('/graphql', graphqlHTTP({
+   
+}))
 
 app.listen(4000, () => {
     console.log("now listening for request on port 4000")
