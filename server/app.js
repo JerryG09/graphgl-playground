c69efd66e239d55cc2f2eec1c9029806bd7cd7f7
JerryG09 <phemmy09@gmail.com>
2019-11-05 07:23:55 +0100

    added book schema

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
-import express from "express"
-import graphqlHTTP from 'express-graphql'
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
 
 const app = express()
 
