afba443968f42ce0bddfa50f0146337257b15d6b
JerryG09 <phemmy09@gmail.com>
2019-11-06 07:28:13 +0100

    added AddBook Component

---
+++ client/src/components/AddBook.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { gql } from "apollo-boost"
+import { graphql } from "react-apollo"
+
+const getAuthorsQuery = gql`
+  {
+    authors{
+      name
+      id
+    }
+  }
+`
+
+function AddBook(props) {
+    // console.log(props)
+    function displayAuthors() {
+      const data = props.data;
+      if (data.loading) {
+        return (<option disabled>Loading Authors...</option>);
+      }
+
+      if(!data.loading) {
+        return data.authors.map(author => {
+          return (<option key={author.id} value={author.id}>{author.name}</option>)
+        })
+      }
+    }
+  
+    return (
+      <div>
+        <form id="add-book">
+          <div className="field">
+            <label htmlFor="">Book name:</label>
+            <input type="text"/>
+          </div>
+
+          <div className="field">
+            <label htmlFor="">Genre:</label>
+            <input type="text"/>
+          </div>
+
+          <div className="field">
+            <label htmlFor="">Author:</label>
+            <select name="" id="">
+              <option value="">Select Author</option>
+              {displayAuthors()}
+            </select>
+          </div>
+
+          <button>+</button>
+        </form>
+      </div>
+    );
+  }
+
+
+export default graphql(getAuthorsQuery)(AddBook);
