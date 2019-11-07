997dd653fd8a4dff176df13dbae49ac38cb98a7a
JerryG09 <phemmy09@gmail.com>
2019-11-07 06:44:58 +0100

    backup

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { graphql } from "react-apollo"
 import { getAuthorsQuery } from "../querries/querries"
 
 function AddBook(props) {
-    // console.log(props)
+  const initialBook = {
+    name: "",
+    genre: "",
+    authorId: ""
+  }
+
+  const [books, setBooks] = useState(initialBook)
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    const value = e.target.value;
+    const name = e.target.name
+    setBooks({...books, [name]: value})
+    console.log(books)
+  }  
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    if (!books.authorId.length || !books.genre.length || !books.name.length) {
+      return
+    }
+
+    // else {
+
+    // }
+    console.log()
+  }
+
     function displayAuthors() {
       const data = props.data;
       if (data.loading) {
@@ -19,21 +47,36 @@ function AddBook(props) {
   
     return (
       <div>
-        <form id="add-book">
+        <form onSubmit={handleSubmit}>
           <div className="field">
             <label htmlFor="">Book name:</label>
-            <input type="text"/>
+            <input 
+              type="text" 
+              onChange={handleChange}
+              name="name"
+              value={books.name}
+            />
           </div>
 
           <div className="field">
             <label htmlFor="">Genre:</label>
-            <input type="text"/>
+            <input 
+              type="text" 
+              onChange={handleChange}
+              name="genre"
+              value={books.genre}
+            />
           </div>
 
           <div className="field">
             <label htmlFor="">Author:</label>
-            <select name="" id="">
-              <option value="">Select Author</option>
+            <select 
+              type="text" 
+              onChange={handleChange}
+              name="authorId"
+              value={books.authorId}
+            >
+              <option name="">Select Author</option>
               {displayAuthors()}
             </select>
           </div>
